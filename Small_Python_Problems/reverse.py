136ffcdc5f05d60b12c9faa0971f3d3f3310e246
Ratheshan03 <lionratheshan@gmail.com>
2021-09-10 01:46:21 +0530

    unique_list

--- Small_Python_Problems/reverse.py
+++ Small_Python_Problems/reverse.py
@@ -20,3 +20,19 @@ def factors(x):
     return factor_list
 
 print(factors(21))
+
+
+#  Returning unique list
+
+def Uniques(list):
+    unique_list = []
+    for x in list:
+        if x not in unique_list:
+            unique_list.append(x)
+    
+    return unique_list
+
+print(Uniques([1,2,3,4,2,4,5,3,2,1,5,5]))
+
+
+# 
