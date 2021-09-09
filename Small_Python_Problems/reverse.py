bf0a37d86b44550028f6b3d6fd220cdad88f09fa
Ratheshan03 <lionratheshan@gmail.com>
2021-09-10 01:36:48 +0530

    factor_checker

--- Small_Python_Problems/reverse.py
+++ Small_Python_Problems/reverse.py
@@ -8,3 +8,15 @@ def ReverseWords(string):
 
 
 print(ReverseWords("APPLE"))
+
+
+#  Factors of a given number
+
+def factors(x):
+    factor_list = []
+    for i in range(1,x+1):
+        if (x % i == 0 ):
+            factor_list.append(i)
+    return factor_list
+
+print(factors(21))
