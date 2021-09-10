e3aff3c0d8a50a33e6bd9198b9559a7a314fce3f
Ratheshan03 <lionratheshan@gmail.com>
2021-09-10 20:21:10 +0530

    Primess

---
+++ Small_Python_Problems/isPrime.py
@@ -0,0 +1,33 @@
+# Checking if its a prime number or not
+
+def isPrime(x):
+    """Returns whether or not the given number x is prime.
+
+    A prime number is a natural number greater than 1 that cannot be formed
+    by multiplying two smaller natural numbers.
+
+    For example:
+    - Calling isPrime(11) will return True
+    - Calling isPrime(71) will return True
+    - Calling isPrime(12) will return False
+    - Calling isPrime(76) will return False
+    """
+    
+    # your code here
+    prime = False
+    
+    if x > 1:
+        # Iterate from 2 to n / 2
+        for i in range(2, int(x/2)+1):
+
+            # If num is divisible by any number between
+            # 2 and n / 2, it is not prime
+            if (x % i) == 0:
+                prime = False
+                break
+        else:
+            prime = True
+    else:
+        prime = False
+        
+    return prime
