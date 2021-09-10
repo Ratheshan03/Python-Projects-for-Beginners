2c10d67fe664031ddbfdde08f1ce35f18170df38
Ratheshan03 <lionratheshan@gmail.com>
2021-09-10 20:22:00 +0530

    reverse_words

--- Small_Python_Problems/reverse.py
+++ Small_Python_Problems/reverse.py
@@ -10,27 +10,7 @@ def ReverseWords(string):
 print(ReverseWords("APPLE"))
 
 
-#  Factors of a given number
-
-def factors(x):
-    factor_list = []
-    for i in range(1,x+1):
-        if (x % i == 0 ):
-            factor_list.append(i)
-    return factor_list
-
-print(factors(21))
 
 
-#  Returning unique list
 
-def Uniques(list):
-    unique_list = []
-    for x in list:
-        if x not in unique_list:
-            unique_list.append(x)
-    
-    return unique_list
-
-print(Uniques([1,2,3,4,2,4,5,3,2,1,5,5]))
 
