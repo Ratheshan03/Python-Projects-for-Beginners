f79039c7dfe279bea86da8644472f5e5f504cc66
Ratheshan03 <lionratheshan@gmail.com>
2021-09-10 20:21:48 +0530

    Narcissistic_numbers

---
+++ Small_Python_Problems/Narcissistic.py
@@ -0,0 +1,33 @@
+def isNarcissistic(x):
+    """Returns whether or not a given number is Narcissistic.
+
+    A positive integer is called a narcissistic number if it
+    is equal to the sum of its own digits each raised to the
+    power of the number of digits.
+
+    Example: 153 is narcissistic because 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153.
+    Note that by this definition all single digit numbers are narcissistic.
+    """
+    
+    # your code here
+    narci = False
+    sum = 0
+    length = len(str(x))
+ 
+    # Traversing through the string
+    for i in str(x):
+        # Converting character to int
+        sum = sum + int(i) ** length
+ 
+    # Converting string to integer
+    number = int(x)
+ 
+    # Comparing number and sum
+    if (number == sum):
+        narci = True
+    else:
+         narci = False
+    
+    return narci
+    
+
