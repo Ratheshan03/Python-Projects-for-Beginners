5ef9a5f97724c28cb8e2afc066369dccbf563f6c
Ratheshan03 <lionratheshan@gmail.com>
2021-09-10 20:20:47 +0530

    Perfectss

---
+++ Small_Python_Problems/isPerfect.py
@@ -0,0 +1,26 @@
+def isPerfect(x):
+    """Returns whether or not the given number x is perfect.
+
+    A number is said to be perfect if it is equal to the sum of all its
+    factors (for obvious reasons the list of factors being considered does
+    not include the number itself).
+
+    Example: 6 = 3 + 2 + 1, hence 6 is perfect.
+    Example: 28 is another example since 1 + 2 + 4 + 7 + 14 is 28.
+    Note, the number 1 is not a perfect number.
+    """
+    
+    # your code here
+    
+    perfect = False
+    sum = 0
+    for i in range(1, x):
+        if(x % i == 0):
+            sum += i
+    if (sum == x):
+        perfect = True
+        
+    else:
+        perfect = False
+        
+    return perfect
