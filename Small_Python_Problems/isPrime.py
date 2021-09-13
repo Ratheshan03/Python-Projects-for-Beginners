815ea5996c4c2cc602b7842babdb3dc5953d881c
Ratheshan03 <lionratheshan@gmail.com>
2021-09-13 13:26:53 +0530

    added-new func

--- Small_Python_Problems/isPrime.py
+++ Small_Python_Problems/isPrime.py
@@ -31,3 +31,22 @@ def isPrime(x):
         prime_num = False
         
     return prime_num
+
+
+# Prime number checking function-2
+
+def isPrime2(array):
+    for num in array:
+    # search for factors, iterating through numbers ranging from 2 to the number itself
+        for i in range(2, num):
+
+    # number is not prime if modulo is 0
+            if (num % i) == 0:
+                print("{} is NOT a prime number, because {} is a factor of {}".format(num, i, num))
+                break
+
+    # otherwise keep checking until we've searched all possible factors, and then declare it prime
+            if i == num -1:    
+                print("{} IS a prime number".format(num))
+
+    
