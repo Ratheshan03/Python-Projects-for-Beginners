6042296535247c9642d8d64b6e51b0bd1e8c5acf
Ratheshan03 <lionratheshan@gmail.com>
2021-09-17 23:28:29 +0530

    generate_functions

--- Small_Python_Problems/Own_genraterfunc.py
+++ Small_Python_Problems/Own_genraterfunc.py
@@ -11,7 +11,7 @@
 
 lessons = ["Why Python Programming", "Data Types and Operators", "Control Flow", "Functions", "Scripting"]
 
-def my_enumerate(iterable, start=0):
+def enumerate(iterable, start=0):
     # Implement your generator function here
     count = start
     for element in iterable:
@@ -19,5 +19,5 @@ def my_enumerate(iterable, start=0):
         count += 1
 
 
-for i, lesson in my_enumerate(lessons, 1):
+for i, lesson in enumerate(lessons, 1):
     print("Lesson {}: {}".format(i, lesson))
